56d07b63a9b702af085df4d08bbb4dbd3cb1a7ea
mithunvoe <kabyasaha1812@gmail.com>
2023-12-03 02:09:59 +0600

    fixed bigMultiplication

--- Miscellaneous/bigMultiplication.cpp
+++ Miscellaneous/bigMultiplication.cpp
@@ -18,36 +18,7 @@ void reverse(char s[], int ln)
     }
     s[ln] = '\0';
 }
-void bigsum(char s1[], char s2[], char ans[])
-{
-    int l1 = 0, l2 = 0;
-    l1 = strlen(s1);
-    l2 = strlen(s2);
-    reverse(s1, l1);
-    reverse(s2, l2);
-    int carry = 0, sm = 0, n = max(l1, l2);
-    for (int i = 0; i < n; i++)
-    {
-        sm = 0;
-        if (i >= l1 || i >= l2)
-        {
-            if (i >= l1)
-                sm += s2[i] - '0';
-            else
-                sm += s1[i] - '0';
-        }
-        else
-        {
-            sm += s2[i] - '0' + s1[i] - '0';
-        }
-        sm += carry;
-        carry = sm / 10;
-        sm %= 10;
-        ans[i] = sm + '0';
-    }
-    reverse(ans, n);
-    // printf("%s", ans);
-}
+
 void kire()
 {
     char s1[101], s2[101], ans[10001], ansx[10001];
@@ -64,20 +35,21 @@ void kire()
     reverse(s2, l2);
     int carry = 0, sm = 0, n = max(l1, l2);
     int k = 0;
-    // printf("%s %s ", s1, s2);
     for (int i = 0; i < l1; i++)
     {
         k = i;
+        carry = 0;
         for (int j = 0; j < l2; j++, k++)
         {
-            sm = ans[k] + (s2[j] - '0') * (s1[i] - '0') + carry;
-            printf("%d\n", sm);
+            sm = ans[k] - '0' + (s2[j] - '0') * (s1[i] - '0') + carry;
             carry = sm / 10;
+
             sm %= 10;
             ans[k] = sm + '0';
-
-            // printf("%d %d\n", sm, carry);
         }
+        ans[k] += carry;
+        if (carry)
+            k++;
     }
     reverse(ans, k);
     ans[k] = '\0';
@@ -87,10 +59,10 @@ void kire()
 int main()
 {
     int t = 1;
-    // scanf("%d", &t);
+
     for (int i = 1; i <= t; i++)
     {
-        // printf("Case %d: ",i );
+
         kire();
     }
 }
